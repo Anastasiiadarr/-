f2acad388942812815ccbc8135ec1d9083a996bf
Anastasiiadarr <anastasiiadar@gmail.com>
2025-05-25 23:43:50 +0300

    Add files via upload

---
+++ лр/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Привіт, Vercel + React!</h1>
+      <p>Привіт</p>
+    </div>
+  );
+}
+
+export default App;
